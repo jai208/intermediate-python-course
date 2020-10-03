27af7652cef83cc9bd22adf5f9f8b81af710931d
Jai kumar <jai.kr208@gmail.com>
2020-10-03 09:45:02 +0530

    Complete with user input

--- dice_roller.py
+++ dice_roller.py
@@ -1,12 +1,17 @@
 import random
 def main():
-  dice_rolls=2
   dice_sum=0
+  dice_rolls= int(input("How many dice do you want to roll?"))
   for i in range(0,dice_rolls):
-     
-     rnd=random.randint(1,6)
-     print(f'{i}.You rolled a {rnd} ')
-     dice_sum=dice_sum+rnd
-     print(f"The sum is {dice_sum}")
+    rnd=random.randint(1,6)
+    dice_sum+=rnd 
+    if (rnd ==1):
+         print(f"{i}.You rolled a {rnd},!Critical Failure")
+    elif (rnd==6):
+         print(f"{i}.You rolled a {rnd},!Critical Success")
+    else :  
+         print(f'{i}.You rolled a {rnd} ')
+    
+  print(f" The sum is {dice_sum}")      
 if __name__== "__main__":
   main()
